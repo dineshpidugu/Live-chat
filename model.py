686f955ef3e8ff280f84967ee04dba69b903fa66
Dinesh <pidugudinesh05@gmail.com>
2026-05-31 22:33:41 +0530

    model error to add id

--- model.py
+++ model.py
@@ -1,31 +1,37 @@
-from sqlalchemy import Column, Integer, String, BigInteger, JSON
+from sqlalchemy import Column, Integer, String, BigInteger, JSON, ForeignKey
 from sqlalchemy.orm import declarative_base
 
 Base = declarative_base()
+
 
 class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String)
-    username=Column(String)
-    password=Column(String)
-class Room(Base):
-    __tablename__="room"
+    name = Column(String, nullable=False)
+    username = Column(String, unique=True, nullable=False)
+    password = Column(String, nullable=False)
 
-    id=Column(Integer,primary_key=True,index=True)
-    roomname=Column(String)
-    # chatid=Column(Integer)
-    password=Column(Integer)
+
+class Room(Base):
+    __tablename__ = "room"
+
+    id = Column(Integer, primary_key=True, index=True)
+    roomname = Column(String, unique=True, nullable=False)
+    password = Column(String)
+
 
 class Chat(Base):
-    __tablename__ = "chat"  
+    __tablename__ = "chat"
+
     id = Column(BigInteger, primary_key=True)
     content = Column(JSON)
 
-class UserRoom(Base):
-    __tablename__ ="userroom"
 
-    user_name = Column(Integer)
-    room_id = Column(Integer)
-    room_name = Column(String)
+class UserRoom(Base):
+    __tablename__ = "userroom"
+
+    id = Column(Integer, primary_key=True)
+
+    user_id = Column(Integer, ForeignKey("users.id"))
+    room_id = Column(Integer, ForeignKey("room.id"))
